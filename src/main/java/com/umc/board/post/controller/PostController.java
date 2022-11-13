ff81fa19e034fe3b471138508f31a60fb8db56bf
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-13 23:01:10 +0900

    Feat: 새로운 게시물 작성 컨트롤러 구현

--- src/main/java/com/umc/board/post/controller/PostController.java
+++ src/main/java/com/umc/board/post/controller/PostController.java
@@ -1,11 +1,15 @@
 package com.umc.board.post.controller;
 
+import com.umc.board.post.model.PostReq;
 import com.umc.board.post.model.PostRes;
+import com.umc.board.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -13,13 +17,16 @@ import java.util.List;
 @Controller
 public class PostController {
 
+    private final PostService postService;
+
     @GetMapping("/posts/all")
     public ResponseEntity<List<PostRes>> getAllPosts() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(postService.getAllPosts());
     }
 
     @PostMapping("posts/new")
-    public ResponseEntity<Void> createNewPost() {
-        ;
+    public ResponseEntity<Void> createNewPost(@RequestBody PostReq postReq) {
+        postService.createPost(postReq);
+        return ResponseEntity.ok().build();
     }
 }

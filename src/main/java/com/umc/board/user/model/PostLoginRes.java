35c31b3d211ffd475fead07d918f0005a5217c29
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-15 01:27:33 +0900

    Feat: 로그인 요청 반환 객체 생성

---
+++ src/main/java/com/umc/board/user/model/PostLoginRes.java
@@ -0,0 +1,13 @@
+package com.umc.board.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PostLoginRes {
+
+    private String nickname;
+}

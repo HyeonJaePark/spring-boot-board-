6d9d962b004ae410e30a47df69305dcff9217702
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-15 01:28:15 +0900

    Feat: spring-security configuration 설정

---
+++ src/main/java/com/umc/board/user/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.umc.board.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // post 방식으로 값을 전송할 때 token을 사용해야하는 보안 설정을 해제
+                .authorizeRequests()
+                .antMatchers("/", "/users/join", "/**").permitAll()
+                .anyRequest().authenticated();
+    }
+}

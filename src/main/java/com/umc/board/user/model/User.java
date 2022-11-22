1fb42d5d402e339a3cf70a5dafd3a70ba4264dee
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-22 14:11:09 +0900

    Refactor: 전체 라이브러리 import 방식으로 수정

--- src/main/java/com/umc/board/user/model/User.java
+++ src/main/java/com/umc/board/user/model/User.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
